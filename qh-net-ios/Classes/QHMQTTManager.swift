fcaf0f22202f889b794c33bc53a5dbb392ddca97
高炀辉 <insistgyh@foxmail.com>
2021-01-09 14:26:10 +0800

    QHMQTT封装完成
    - QHMQTTManager 封装完成

--- qh-net-ios/Classes/QHMQTTManager.swift
+++ qh-net-ios/Classes/QHMQTTManager.swift
@@ -7,14 +7,25 @@
 
 import Foundation
 import MQTTClient
+
+/// 代理协议
+public protocol QHMQTTManagerDelegate {
+    /// 连接状态已经改变
+    func sessionManager(_ sessionManager: MQTTSessionManager!, didChange newState: MQTTSessionManagerState)
+    /// 回调消息
+    func handleMessage(_ data: Data!, onTopic topic: String!, retained: Bool)
+}
 /// MQTT 管理器
 /// 对 MQTTClient 封装
-public struct QHMQTTManager {
+public class QHMQTTManager: NSObject {
     /// 初始化 QHMQTTManager
     public static let manager = QHMQTTManager()
     
     /// MQTTSessionManager
     private let QHMQTTSessionManager = MQTTSessionManager()
+    
+    /// 链接代理
+    public var delegate:QHMQTTManagerDelegate?
     
     /// 是否链接成功
     public var isConnect: Bool {
@@ -24,9 +35,8 @@ public struct QHMQTTManager {
         default:
             return false
         }
-        return false
     }
-    
+
     /// 链接MQTT
     public func connect(
         to host: String?,
@@ -49,14 +59,32 @@ public struct QHMQTTManager {
         connectHandler: @escaping MQTTConnectHandler
     ) {
         self.QHMQTTSessionManager.connect(to: host, port: port, tls: tls, keepalive: keepalive, clean: clean, auth: auth, user: user, pass: pass, will: will, willTopic: willTopic, willMsg: willMsg, willQos: willQos, willRetainFlag: willRetainFlag, withClientId: withClientId, securityPolicy: securityPolicy, certificates: certificates, protocolLevel: protocolLevel, connectHandler: connectHandler)
+        self.QHMQTTSessionManager.delegate = self
     }
     /// 断开链接
     public func disconnect() {
-//        if self.QHMQTTSessionManager
         self.QHMQTTSessionManager.disconnect { error in
             print("链接断开: error:", error.debugDescription)
         }
         self.QHMQTTSessionManager.delegate = nil
-        self.QHMQTTSessionManager = nil
+    }
+    /// 发布消息
+    func send(_ data: Data?, topic: String?, qos: MQTTQosLevel, retain retainFlag: Bool) {
+        self.QHMQTTSessionManager.send(data, topic: topic, qos: qos, retain: retainFlag)
+    }
+}
+/// 代理设置
+extension QHMQTTManager: MQTTSessionManagerDelegate {
+    public func sessionManager(_ sessionManager: MQTTSessionManager!, didChange newState: MQTTSessionManagerState) {
+        guard let delegate = self.delegate else {
+            return
+        }
+        delegate.sessionManager(sessionManager, didChange: newState)
+    }
+    public func handleMessage(_ data: Data!, onTopic topic: String!, retained: Bool) {
+        guard let delegate = self.delegate else {
+            return
+        }
+        delegate.handleMessage(data, onTopic: topic, retained: retained)
     }
 }
